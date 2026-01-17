d10f9b18e756dc680bd3ec5c12c8d46d8ba320db
Ashler Herrick <ashlerjherrick@gmail.com>
2026-01-17 15:21:40 -0500

    script

--- scripts/test_tc.py
+++ scripts/test_tc.py
@@ -2,7 +2,7 @@ from theta_client.client import ThetaClient, MinIOConfig
 from theta_client.requests import OptionRequest, DataType, Endpoint, FileGranularity
 from theta_client.utils import get_symbol_universe
 
-# univ = get_symbol_universe()
+univ = get_symbol_universe()
 
 # Create client with log level configuration
 # Options: "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"
@@ -12,15 +12,15 @@ client = ThetaClient(
     log_level="DEBUG",  # Change to "DEBUG" for verbose output, "WARNING" for quiet
 )
 
-# for symbol in univ:
-req = OptionRequest(
-    symbol="AAPL",
-    start_date=20190201,
-    end_date=20190228,
-    data_type=DataType.HISTORY,
-    endpoint=Endpoint.GREEKS_EOD,
-    file_granularity=FileGranularity.MONTHLY,
-    force_refresh=False,
-)
+for symbol in univ:
+    req = OptionRequest(
+        symbol=symbol,
+        start_date=20160101,
+        end_date=20251231,
+        data_type=DataType.HISTORY,
+        endpoint=Endpoint.GREEKS_EOD,
+        file_granularity=FileGranularity.MONTHLY,
+        force_refresh=False
+    )
 
-client.request_data(req)
+    client.request_data(req)
